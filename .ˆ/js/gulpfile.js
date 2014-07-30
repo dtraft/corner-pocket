886f7e497f64944796a3fdede7926b81549a2505
dtraft <dtraft@gmail.com>
2014-07-30 11:30:02 -0400

    formatting

--- .ˆ/js/gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var gutil = require('gulp-util');
-var prettify = require('gulp-jsbeautifier');
-gulp.task('format', function() {
-    gulp.src('./*.js').pipe(prettify({
-        config: '.jsbeautifyrc'
-    })).pipe(gulp.dest('.ˆ/js'))
-});
